e5f5840093d0f354339a70c1b77306c9c048d4bf
Justin Love <git@JustinLove.name>
2014-11-17 21:16:50 -0600

    add 1v1test biome

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 1.1.0
+
+- Add 1v1test biome from 75134-main
--- ui/mods/hidden_biomes/system_editor.js
+++ ui/mods/hidden_biomes/system_editor.js
@@ -1,1 +1,1 @@
-model.biomes.push('csg_debug', 'ice_boss', 'metal_boss', 'sandbox')
+model.biomes.push('1v1test', 'csg_debug', 'ice_boss', 'metal_boss', 'sandbox')
